9571973437638a1778e2dc2576d54b2e8172c5b0
madiha mamdouh <madmamdouh@gmail.com>
2022-08-18 13:36:27 +0200

    add apis

--- client/src/components/Table/Order.js
+++ client/src/components/Table/Order.js
@@ -1,92 +1,76 @@
 import * as React from "react";
-import { DataGrid } from "@mui/x-data-grid";
 import "./order.scss";
-import { useState, useEffect } from "react";
+import Table from "@mui/material/Table";
+import TableBody from "@mui/material/TableBody";
+import TableCell from "@mui/material/TableCell";
+import TableContainer from "@mui/material/TableContainer";
+import TableHead from "@mui/material/TableHead";
+import TableRow from "@mui/material/TableRow";
+import Paper from "@mui/material/Paper";
+import { useState } from "react";
 import { userRequest } from "../../requestApi";
-
-const columns = [
-  { field: "id", headerName: "ID", width: 200 },
-  { field: "customer", headerName: "customer-Id", width: 200 },
-  { field: "quantity", headerName: "Quantity", width: 130 },
-  { field: "total", headerName: "TotalAmount", width: 130 },
-  { field: "address", headerName: "Address", width: 100 },
-  {
-    field: "status",
-    headerName: "Status",
-    type: "string",
-    width: 100,
-  },
-];
+import { useEffect } from "react";
 
 export default function Order() {
   const [orders, setOrder] = useState([]);
-  const [rowOrders, setRowOrders] = useState([]);
-  const handleDelete = async (id) => {
-    try {
-      const res = await userRequest.delete("/orders/" + id);
-      console.log(res.data);
-    } catch (err) {
-      console.log(err);
-    }
-  };
+
+  // const handleDelete = async (id) => {
+  //   try {
+  //     const res = await userRequest.delete("/orders/" + id);
+  //     console.log(res.data);
+  //   } catch (err) {
+  //     console.log(err);
+  //   }
+  // };
 
   useEffect(() => {
     const getOrder = async () => {
       try {
-        const res = await userRequest.get("/orders");
+        const res = await userRequest.get("orders");
         setOrder(res.data);
       } catch (err) {
         console.log(err);
       }
     };
     getOrder();
-
-    let rowOrder = [];
-    orders.forEach((order) => {
-      let cellOrder = {
-        ...order,
-        id: order._id,
-        customer: order.userId,
-        quantity: order.products[0].quantity,
-        total: `$${order.amount}`,
-        address: order.address,
-        status: order.status,
-      };
-      rowOrder.push(cellOrder);
-    });
-    setRowOrders(rowOrder);
   }, []);
-
-  const actionColumn = [
-    {
-      field: "action",
-      headerName: "Action",
-      width: 100,
-      renderCell: (params) => {
-        return (
-          <div className="cellAction">
-            <div
-              className="deleteButton"
-              onClick={() => handleDelete(params.row.id)}
-            >
-              Delete
-            </div>
-          </div>
-        );
-      },
-    },
-  ];
-
+  console.log(orders);
   return (
-    <div className="order" style={{ height: 480, width: "100%" }}>
-      <DataGrid
-        id={Date.now()}
-        rows={rowOrders}
-        columns={columns.concat(actionColumn)}
-        pageSize={7}
-        rowsPerPageOptions={[7]}
-        checkboxSelection
-      />
-    </div>
+    <TableContainer component={Paper} className="table">
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            <TableCell className="tableCell">User ID</TableCell>
+            <TableCell className="tableCell">Product ID</TableCell>
+            <TableCell className="tableCell">Quantity</TableCell>
+            <TableCell className="tableCell">Total </TableCell>
+            <TableCell className="tableCell">Address</TableCell>
+            <TableCell className="tableCell">Status </TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>
+          {orders.map((row) => (
+            <TableRow key={row._id}>
+              <TableCell className="tableRow">{row.userId}</TableCell>
+              <TableCell className="tableRow">
+                <div className="productWrapper">
+                  <img src={row.img} alt="" className="productImg" />
+                  {row.username}
+                </div>
+              </TableCell>
+              <TableCell className="tableRow">
+                {row.products[0].productId}
+              </TableCell>
+              <TableCell className="tableRow">
+                {row.products[0].quantity}
+              </TableCell>
+              <TableCell className="tableRow">{row.amount}</TableCell>
+              <TableCell className="tableRow">{row.address}</TableCell>
+              <TableCell className="tableRow">{row.stauts}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
   );
 }

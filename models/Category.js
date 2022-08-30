0d9f9203c66ed97b5e9a91320e3d9bf6b776aede
madiha mamdouh <madmamdouh@gmail.com>
2022-08-30 13:01:32 +0200

    al done

--- models/Category.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require("mongoose");
-const CategorySchema = new mongoose.Schema(
-  {
-    title: { type: String, required: true, unique: true },
-    products: { type: Array },
-  },
-  { timestamps: true }
-);
-module.exports = mongoose.model("Category", CategorySchema);
